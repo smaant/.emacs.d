bab9484f5548e5f5b1514e49db4527f672952481
Anton Panferov <smaant@gmail.com>
2015-09-13 00:35:45 +0300

    Rainbow delimieters colors

--- init.el
+++ init.el
@@ -147,3 +147,23 @@
 ;; Langauage-specific
 (load "setup-clojure.el")
 (load "setup-js.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "purple3"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "forest green"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "magenta4"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "DodgerBlue2"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "SlateBlue2"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "brown"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "DeepSkyBlue1"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "purple"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "SlateGray4")))))
